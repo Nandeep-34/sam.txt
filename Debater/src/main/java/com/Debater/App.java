7b971154e82df3de1152cda897e50240c9adbc88
Nandeep-34 <krnandeep@gmail.com>
2026-07-20 18:51:49 +0530

    Kuiper

---
+++ Debater/src/main/java/com/Debater/App.java
@@ -0,0 +1,10 @@
+package com.Debater;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
---
+++ Starshield/src/main/java/com/Starshield/App.java
@@ -0,0 +1,10 @@
+package com.Starshield;
+
+/**
+ * Hello world!
+ */
+public class App {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
